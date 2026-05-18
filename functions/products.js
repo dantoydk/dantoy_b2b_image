7a2a989b11c25f2cc9916a2b9deef8a40ec2469e
rstmsdnty <158029743+rstmsdnty@users.noreply.github.com>
2026-05-18 09:51:29 +0200

    try without origin

--- functions/products.js
+++ functions/products.js
@@ -79,7 +79,7 @@ export async function onRequest(context) {
   }
 
   // ✅ Only allow GET + valid origin + auth
-  if (method === "GET" && origin && allowedAuth) {
+  if (method === "GET" && allowedAuth) {
     try {
       const token = await getShopwareApiToken(clientId, clientSecret);
 
